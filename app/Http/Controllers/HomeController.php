cbbefdba81d089e36198e1c47289ff3b72d9917d
kontrasenyas <alcantarajosephc@gmail.com>
2017-04-15 09:36:52 +0800

    Edit latest post

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,16 +1,13 @@
 <?php
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Post;
-use App\Location;
-use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
 	public function getHome()
 	{
-		$posts = Post::orderBy('created_at', 'desc')->paginate(5);
+		$posts = Post::orderBy('created_at', 'desc')->paginate(6);
 		return view('home', ['posts' => $posts]);
 	}
 }
