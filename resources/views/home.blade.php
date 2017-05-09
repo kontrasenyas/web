e4d94f4bf9ee259093aadec16903052eed277b74
kontrasenyas <alcantarajosephc@gmail.com>
2017-05-09 20:54:58 +0800

    Meta description

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,9 @@
 Libot Philippines
 @endsection
 
+@section('description', 'Libot Philippines helps you to find travel package, a car/services to use for your travel and everything you needs for travel.')
+@section('meta-image', 'https://libot-ph.com/favicon.ico')
+
 @section('content')
 @include('includes.search')
 <hr>
