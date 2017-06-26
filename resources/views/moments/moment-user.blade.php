0634f94b72df864129e45e550c8e27fa7173b0a0
kontrasenyas <alcantarajosephc@gmail.com>
2017-06-26 22:18:29 +0800

    change width and height of moments

--- resources/views/moments/moment-user.blade.php
+++ resources/views/moments/moment-user.blade.php
@@ -1,7 +1,7 @@
 @foreach($moments as $moment)
     <div class="col-md-12 form-group">
         <div class="col-md-6">
-            <img src="{{ route('moment.image', ['filename' => $moment->image]) }}" alt="" class="img-responsive pull-right" width="450px" height="450px">
+            <img src="{{ route('moment.image', ['filename' => $moment->image]) }}" alt="" class="img-responsive pull-right" width="400px" height="400px">
         </div>
         <div class="col-md-6">
             <p class="text-muted"> {{ $moment->location }}</p> 
