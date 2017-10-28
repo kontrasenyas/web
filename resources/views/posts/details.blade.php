b9fbfee5730215e4a9fc9b003191886ab9aa999c
kontrasenyas <alcantarajosephc@gmail.com>
2017-10-29 02:43:44 +0800

    recommit

--- resources/views/posts/details.blade.php
+++ resources/views/posts/details.blade.php
@@ -29,7 +29,7 @@
 		<div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
 			<ol class="breadcrumb">
 				<li><a href="index.html">Dashboard</a></li>
-				<li><a href="#"><span>e-commerce</span></a></li>
+				<li><a href="#"><span>post</span></a></li>
 				<li class="active"><span>post detail</span></li>
 			</ol>
 		</div>
