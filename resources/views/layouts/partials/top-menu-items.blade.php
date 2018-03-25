c22cc8159f0437569cf9bd2378592d5e887b82e1
kontrasenyas <alcantarajosephc@gmail.com>
2018-03-25 17:39:00 +0800

    edit logo

--- resources/views/layouts/partials/top-menu-items.blade.php
+++ resources/views/layouts/partials/top-menu-items.blade.php
@@ -3,7 +3,7 @@
 		<div class="nav-header pull-left">
 			<div class="logo-wrap">
 				<a href="{{ route('home') }}">
-					<img class="brand-img" src="{{ URL::to('dist/img/logo.png') }}" alt="brand" height="21px;" width="22px;" />
+					<img class="brand-img" src="{{ URL::to('dist/img/logo.png') }}" alt="brand" />
 					<span class="brand-text">Libot</span>
 				</a>
 			</div>
