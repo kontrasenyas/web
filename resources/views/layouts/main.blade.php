2fe0469dfe5b5a51688749d702ec80b43b4d8b3b
kontrasenyas <alcantarajosephc@gmail.com>
2018-04-14 23:44:53 +0800

    recomit

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,13 +3,6 @@
 <head>
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-109991283-1"></script>
-    {{-- <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'UA-109991283-1');
-    </script> --}}
 
     <script>
         (function(e,t,n,i,s,a,c){e[n]=e[n]||function(){(e[n].q=e[n].q||[]).push(arguments)}
